ce3909fb1b25899e6f1132149e6f8f6fbf52b659
Adam O'Neil <adamosoftware@gmail.com>
2018-11-28 11:20:18 -0500

    code cleanup

--- Tests/QueryTests.cs
+++ Tests/QueryTests.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Mp3Player.Models.Queries;
 using Postulate.Lite.Core;
 using Postulate.Lite.Core.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Reflection;
 
 namespace Tests
@@ -19,16 +16,17 @@ namespace Tests
 			string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 			return new SqlConnection(connectionString);
 		}
-		
+
 		[TestMethod]
 		public void TestMp3PlayerModelQueries()
 		{
+			// gets all the query test cases from the model assembly, where queries in this solution are defined
 			IEnumerable<ITestableQuery> queries = QueryTesting.GetTestCases(Assembly.GetExecutingAssembly(), "Mp3Player.Models");
 
 			using (var cn = GetConnection())
-			{				
+			{
 				foreach (var qry in queries) qry.TestExecute(cn);
-			}				
+			}
 		}
 	}
 }
